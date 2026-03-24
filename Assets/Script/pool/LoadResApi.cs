0f5a497b435074eb86cb02b37e364fc763d7b695
yangjie <354948200@qq.com>
2026-03-24 15:22:20 +0800

    先注释部分代码 保证jenkins打包正常

--- Assets/Script/pool/LoadResApi.cs
+++ Assets/Script/pool/LoadResApi.cs
@@ -15,27 +15,28 @@ namespace SCG
     {
         public static T LoadRes<T>(string resUrl) where T : UnityEngine.Object
         {
-            T result = null;
-            //if(!resUrl.StartsWith("Assets"))  // 尽量减少判断 因为是对象池 调用比较频繁能省性能则省 
-            //{
-            //    UnityEngine.Debug.LogError("资源必须是Assets开头");
-            //}
+            return null;
+//            T result = null;
+//            //if(!resUrl.StartsWith("Assets"))  // 尽量减少判断 因为是对象池 调用比较频繁能省性能则省 
+//            //{
+//            //    UnityEngine.Debug.LogError("资源必须是Assets开头");
+//            //}
            
-#if UNITY_EDITOR
-            try
-            {
-                result = AssetDatabase.LoadAssetAtPath<T>(resUrl);
-                return result;
-            }
-            catch (Exception e)
-            {
-                UnityEngine.Debug.LogError(e);
-            }
-#else
-             result =  AssetLoader.Instance.LoadAssetSync<T>(resUrl);
-#endif
+//#if UNITY_EDITOR
+//            try
+//            {
+//                result = AssetDatabase.LoadAssetAtPath<T>(resUrl);
+//                return result;
+//            }
+//            catch (Exception e)
+//            {
+//                UnityEngine.Debug.LogError(e);
+//            }
+//#else
+//             result =  AssetLoader.Instance.LoadAssetSync<T>(resUrl);
+//#endif
 
-            return result;
+//            return result;
         }
     }
 }
